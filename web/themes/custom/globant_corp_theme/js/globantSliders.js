e22b3e3e657a85afed4e5a298fd0704dcac7d8b8
Rakesh Falke <rakesh.falke@globant.com>
2017-10-06 00:33:44 +0530

    Added new blocks

--- web/themes/custom/globant_corp_theme/js/globantSliders.js
+++ web/themes/custom/globant_corp_theme/js/globantSliders.js
@@ -1,8 +1,8 @@
 (function ($, Drupal) {
   Drupal.behaviors.sliders = {
     attach: function (context, settings) {
-      $('div.paragraph-text-slider', context).once('paragraph-text-slider').each(function () {
-        $('div.paragraph-text-slider').slick({autoplay: true, autoplaySpeed: 2000, fade: true,dots: false,
+      $('div.paragraph-home-slider', context).once('paragraph-home-slider').each(function () {
+        $('div.paragraph-home-slider').slick({autoplay: true, autoplaySpeed: 2000, fade: true,dots: false,
     prevArrow: false,
     nextArrow: false});
       });
